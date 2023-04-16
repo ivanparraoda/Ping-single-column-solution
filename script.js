b103612015c7d3959113d4548ce547d1d9120d8b
Ivan Parra <98499975+ivanparraoda@users.noreply.github.com>
2023-04-15 23:22:34 -0600

    add validation

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 
-const password = document.getElementById('password')
+const email = document.getElementById('mail')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
+const errorstyle = document.querySelector("input");
+
+
 
 form.addEventListener('submit', (e) => {
   let messages = []
  
-
-  if (password.value.length <= 6) {
-    messages.push('Password must be longer than 6 characters')
+  if (email.value === '' || email.value.length <= 6  || email.value.length >= 20) {
+    messages.push('Please provide a valid email address')
+    
   }
 
-  if (password.value.length >= 20) {
-    messages.push('Password must be less than 20 characters')
-  }
-
-  if (password.value === 'password') {
-    messages.push('Password cannot be password')
-  }
 
   if (messages.length > 0) {
     e.preventDefault()
     errorElement.innerText = messages.join(', ')
   }
 })
+
+
